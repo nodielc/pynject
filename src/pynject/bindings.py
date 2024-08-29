04d696e3733688320d3a66ad04d3b21bc79a6a51
Nodiel <nodiel.clavijo@gmail.com>
2024-08-29 23:57:52 +0100

    add constants bindings

--- src/pynject/bindings.py
+++ src/pynject/bindings.py
@@ -7,8 +7,7 @@ class ConcreteWrapper(typing.Protocol):
         pass
 
 
-class FactoryConcreteWrapper:
-
+class ConcretesFactoryWrapper:
     def __init__(
         self, concrete_type: type, *args: typing.Any, **kwargs: typing.Any
     ) -> None:
@@ -18,6 +17,14 @@ class FactoryConcreteWrapper:
 
     def resolve(self) -> typing.Any:
         return self._concrete_type(*self._args, **self._kwargs)
+
+
+class ConstantsFactoryWrapper:
+    def __init__(self, value: typing.Any) -> None:
+        self.value = value
+
+    def resolve(self) -> typing.Any:
+        return self.value
 
 
 class ConcreteBinding:
@@ -31,6 +38,9 @@ class ConcreteBinding:
         self._abstract_type = abstract_type
 
     def to(self, concrete_type: type, *args: typing.Any, **kwargs: typing.Any) -> None:
-        self._registry[self._abstract_type] = FactoryConcreteWrapper(
+        self._registry[self._abstract_type] = ConcretesFactoryWrapper(
             concrete_type, *args, **kwargs
         )
+
+    def to_constant(self, value: typing.Any) -> None:
+        self._registry[self._abstract_type] = ConstantsFactoryWrapper(value=value)
